587f8f27e9a6d90ef12427c306f2903da544e690
soclarence <clarenceaigbuza@gmail.com>
2024-12-06 00:35:29 +0100

    scroll to top component added

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,12 @@ import Dash from "./views/Dash";
 import Archive from "./views/Archive";
 import { Routes, Route } from "react-router-dom";
 import Og from "./views/Og";
+import ScrollToTop from "./utils/ScrollToTop";
 
 function App() {
   return (
     <div>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Dash />} />
         <Route path="/archive" element={<Archive />} />
